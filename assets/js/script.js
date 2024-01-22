e0a01c80b3b03eeae58aa803b70ff01b5953f834
Zbigniew Szulc <zbigniewszulc@gmail.com>
2024-01-22 01:37:46 +0000

    Declaration of common functions for PHQ-9 and GAD-7

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// Patient Health Questionnaire 9-item depression scale (PHQ-9) 
+function displayQuestion() {
+}
+
+function displayAnswers() {
+}
+
+function collectAnswer() {
+}
+
+function calculateTotal() {
+}
+
+function displayUserAnswers(){
+}
+
+function displayResult() { 
+}
