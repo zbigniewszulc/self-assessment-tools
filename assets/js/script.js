1662de893ad6af5e744d5c6c7e540b9ff259aeb3
Zbigniew Szulc <zbigniewszulc@gmail.com>
2024-01-22 01:50:08 +0000

    Define and declare array of question for PHQ-9 and GAD-7

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,28 @@
 // Patient Health Questionnaire 9-item depression scale (PHQ-9) 
+const phqQuestions = [
+    /*1*/"Little interest or pleasure in doing things", 
+    /*2*/"Feeling down, depressed, or hopeless", 
+    /*3*/"Trouble falling or staying asleep, or sleeping too much",
+    /*4*/"Feeling tired or having little energy",
+    /*5*/"Poor appetite or overeating",
+    /*6*/"Feeling bad about yourself — or that you are a failure or have let yourself or your family down",
+    /*7*/"Trouble concentrating on things, such as reading the newspaper or watching television",
+    /*8*/"Moving or speaking so slowly that other people could have noticed? Or the opposite — being so fidgety or restless that you have been moving around a lot more than usual",
+    /*9*/"Thoughts that you would be better off dead or of hurting yourself in some way"
+];
+
+//Generalised Anxiety Disorder (GAD-7)
+const gadQuestions = [
+    /*1*/"Feeling nervous, anxious or on edge",
+    /*2*/"Not being able to stop or control worrying",
+    /*3*/"Worrying too much about different things",
+    /*4*/"Trouble relaxing",
+    /*5*/"Being so restless that it is hard to sit still",
+    /*6*/"Becoming easily annoyed or irritable",
+    /*7*/"Feeling afraid as if something awful might happen"
+];
+
+//Common functions for PHQ-9 and GAD-7
 function displayQuestion() {
 }
 
